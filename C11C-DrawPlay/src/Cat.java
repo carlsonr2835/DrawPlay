ee3debb6bc230c4655d429566c500c07248b2e97
thiccboimork <118483938+thiccboimork@users.noreply.github.com>
2024-02-16 11:09:19 -0700

    made green body

--- C11C-DrawPlay/src/Cat.java
+++ C11C-DrawPlay/src/Cat.java
@@ -66,6 +66,8 @@ public class Cat {
 		y = catY + BODY_Y;
 		g2.fillPolygon(new int[] {x + 20, x + BODY_X / 2, x + BODY_Y}, new int[] {y+20, y - BODY_Y, y+20}, 3);
 
+		g2.fillRect(x, y, x + BODY_Y, y + 10);
+
 
 		g2.setColor(Color.pink);
 
